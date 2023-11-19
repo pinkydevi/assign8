7564a66df455eb6448c0555e17e113d098d12686
Pinky Devi <pinkydevi456u@gmail.com>
2023-11-19 21:21:14 +0600

    assign8

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+      
+        $name = "Donald Trump";
+        $age = "75";
+
+        
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        
+        $cookie = cookie($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly);
+
+        
+        return response($data)->cookie($cookie)->status(200);
+    }
+}
+?>
